c852d20d0700b51eb4588b6144c0d44462ef664c
javedkhan-IS210 <javed.khan@spsmail.cuny.edu>
2016-10-10 00:22:27 +0000

    added and completed tasks

---
+++ task_01.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" A function to create matches between players"""
+
+def get_matches(players)
+    """ This function creates matchups between players as tuples
+
+    Args:
+        players(list): A list of players.
+
+    Returns:
+        a list of matches stored in a tuple.
+
+    Examples:
+        >>> import task_01
+        >>> task_01.get_matches(['Harry', 'Howard', 'Hugh'])
+        [('Harry', 'Howard'), ('Harry', 'Hugh'), ('Howard', 'Hugh')]
+        
+    """
+
+    player = []
+    for element in iterable enumerate(players):
+        if element in enumerate(players)
+            if not element in enumerate(players)
+                list= (element, iterable)  
+                players.append(list)
+    return player
